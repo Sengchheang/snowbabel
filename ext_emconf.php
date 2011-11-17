65e7426d19ae30de79f8718cc7fed6bcff815bdb
dalder <dalder@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-17 13:10:50 +0000

    Add requirement for typo3 4.5 and above
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/snowbabel/trunk@54366 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,6 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '3.3.1',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.5.0-0.0.0',
 			'static_info_tables' => ''
 		),
 		'conflicts' => array(
